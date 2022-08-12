a547000fe8e8495e49eaa741f6e4843aa2a1fd4d
yoshinorin <yoshinorin.net@outlook.com>
2022-08-12 17:19:52 +0900

    update

--- src/main/scala/cetutorial/fileCopy.scala
+++ src/main/scala/cetutorial/fileCopy.scala
@@ -1,15 +1,47 @@
 package cetutorial
 
 import cats.effect.{IO, Resource}
+import cats.syntax.all._
 import java.io._
 
 object fileCopy {
-  def copy(origin: File, destination: File): IO[Long] =
+  def copy(origin: File, destination: File): IO[Long] = {
+    /*
     inputOutputStreams(origin, destination).use { case(in, out) =>
       transfer(in, out)
     }
+    */
 
-  def transfer(origin: InputStream, destination: OutputStream): IO[Long] = ???
+    // この場合 inputOutputStreams は不要
+    val inIO: IO[InputStream] = IO(new FileInputStream(origin))
+    val outIO: IO[OutputStream] = IO(new FileOutputStream(destination))
+
+    (inIO, outIO)            // リソースの取得
+      .tupled                // (IO[InputStream], IO[OutputStream]) to IO[(InputStream, OutputStream)]
+      .bracket {
+        case (in, out) =>
+          transfer(in, out)  // リソースの使用
+      } {
+        // このコードの場合、stage1 の取得で失敗するとこの stage3は実行されない点に注意
+        // -> bracket より Resource 使うほうが良い
+        case (in, out) =>    // リソースの開放
+          (IO(in.close()), IO(out.close()))
+          .tupled            // (IO[Unit], IO[Unit]) to IO[(Unit, Unit)]
+          .handleErrorWith(_ => IO.unit).void
+      }
+  }
+
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] = {
+    for {
+      amount <- IO.blocking(origin.read(buffer, 0, buffer.size))
+      count  <- if(amount > -1) IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+               else IO.pure(acc)
+    } yield count
+  }
+
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] = {
+    transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
+  }
 
   def inputStream(f: File): Resource[IO, FileInputStream] = {
     Resource.make {
